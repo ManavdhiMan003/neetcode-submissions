772debff7ffad4ce0b0e35c4dc063ff00555f68c
Manav Dhiman <65016580+ManavdhiMan003@users.noreply.github.com>
2026-07-18 02:39:21 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    vector<int> dp;
+    int fun(vector<int> &v,int x){
+        if(x>=v.size()) return 0;
+        if(dp[x]!=-1) return dp[x];
+        return dp[x] = max(fun(v,x+1),fun(v,x+2)+v[x]);
+    }
+public:
+    int rob(vector<int>& v) {
+        int n = v.size();
+        if(n==0) return 0;
+        if(n==1) return v[0];
+        dp.resize(n,-1);
+        dp[0] = v[0];
+        dp[1] = max(v[0],v[1]);
+        int a=v[0],b=max(v[0],v[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = max(b,v[i]+a);
+            a = dp[i-1];
+            b = dp[i];
+        }
+        return dp[n-1];
+    }
+};
+
+
+// x -> skip x+1, rob x+2
